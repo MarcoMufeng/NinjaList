94ea3aaeb47a2f0bc11419f62096a67ecd0d3ab2
MarcoMufeng <85856879+MarcoMufeng@users.noreply.github.com>
2021-06-14 10:58:01 +0800

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,24 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Footer from '../comps/Footer'
+import Navbar from '../comps/Navbar'
+import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+  <>
+    <Head>
+      <title>Ninja List | Home</title>
+      <meta name="keywords" content="ninjas" />
+    </Head>
+    <div>
+      <h1 className={styles.title}>Home</h1>
+      <p className={styles.text}>This is a website.</p>
+      <Link href='/Ninjas'>
+        <a className={styles.btn}>See Ninja List.</a>
+      </Link> 
+    </div>
+  </>
+  )
+}
